83ff9949828efa97fdc48b9f3c215efb2fed0008
emir <emir.jumaliev@gmail.com>
2019-09-27 07:09:25 -0400

    Added data,fixture and finished first step

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Mission, Status, Type
 
-# Register your models here.
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'status', 'created_at']
+    list_filter = ['status', 'type']
+    list_display_links = ['pk', 'summary']
+    search_fields = ['summary', 'description']
+    exclude = []
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Mission)
+admin.site.register(Status)
+admin.site.register(Type)
