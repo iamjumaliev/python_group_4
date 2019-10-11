67761515424d300b9a9ae0110d0baef466326be5
emir <emir.jumaliev@gmail.com>
2019-10-11 08:28:36 -0400

    was added forms, templates for project

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from webapp.models import Mission, Status,Type
+from webapp.models import Mission, Status, Type, Project
 
 
 class MissionForm(forms.ModelForm):
@@ -20,3 +20,8 @@ class TypeForm(forms.ModelForm):
     class Meta:
         model = Type
         fields = ['type']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['created_at', 'updated_at']
