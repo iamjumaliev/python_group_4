d51643cc373500f0ed64ef45d1f0353f21267dd3
emir <emir.jumaliev@gmail.com>
2019-10-14 09:59:50 -0400

    ProjectView and project.html was improved

--- source/webapp/views/project_view.py
+++ source/webapp/views/project_view.py
@@ -1,4 +1,6 @@
-from webapp.forms import  ProjectForm
+from django.core.paginator import Paginator
+
+from webapp.forms import  ProjectForm,MissionForm
 from webapp.models import Project
 from django.views.generic import ListView,CreateView,DeleteView,UpdateView,DetailView
 from django.urls import reverse, reverse_lazy
@@ -21,8 +23,24 @@ class ProjectIndexView(ListView):
 class ProjectView(DetailView):
     template_name = 'project/project.html'
     model = Project
-    context_object_name = 'project'
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = self.object
+        context['form'] = MissionForm()
+        missions = project.mission_project.order_by('-created_at')
+        self.paginate_mission_project_to_context(missions, context)
+        return context
+
+    def paginate_mission_project_to_context(self, missions, context):
+        paginator = Paginator(missions, 3, 0)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        context['paginator'] = paginator
+        context['page_obj'] = page
+        context['missions'] = page.object_list
+        context['is_paginated'] = page.has_other_pages()
 
 
 class ProjectCreateView(CreateView):
