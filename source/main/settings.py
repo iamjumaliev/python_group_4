9cf6e206b69c8778969d94a048b3dbd6844ee680
emir <emir.jumaliev@gmail.com>
2019-10-28 08:08:54 -0400

    was added base template to lpogin.html was added urls to settings.py

--- source/main/settings.py
+++ source/main/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "accounts:login"
+
+LOGIN_REDIRECT_URL = "webapp:article_index"
+
+LOGOUT_REDIRECT_URL = "webapp:article_index"
