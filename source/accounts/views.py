47a3b2e806a0a3cd92e50c9f28fe3422fd631d4a
emir <emir.jumaliev@gmail.com>
2019-10-28 08:15:48 -0400

    was added new view for ueser (register_view)

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from .forms import UserCreationForm
 
 
 def login_view(request):
@@ -20,4 +20,24 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:login')
+
+def register_view(request, *args, **kwargs):
+
+    if request.method == 'POST':
+
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('webapp:article_index')
+
+    else:
+
+        form = UserCreationForm()
+
+    return render(request, 'user_create.html', context={'form': form})
 # Create your views here.
