079fb89dda12674654b8b68b78c9ac3821bf6c4f
emir <emir.jumaliev@gmail.com>
2019-10-29 09:26:56 -0400

    was urls for detail view

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register_view
+from .views import register_view, UserDetailView
 
 app_name = 'accounts'
 
@@ -8,6 +8,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('create/', register_view, name='create')
+    path('create/', register_view, name='create'),
+    path('profile/<pk>/', UserDetailView.as_view(), name='user_detail'),
 ]
 
