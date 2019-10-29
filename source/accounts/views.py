429af62ad26ec7d1e392dac70c8f99aa6494646a
emir <emir.jumaliev@gmail.com>
2019-10-29 09:20:59 -0400

    was added template for detail view

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import DetailView
+
 from .forms import UserCreationForm
 
 
