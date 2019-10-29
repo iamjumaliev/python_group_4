431d407f8931e43ff98309c60dfa83de40a7d526
emir <emir.jumaliev@gmail.com>
2019-10-29 10:54:58 -0400

    was made UsersList

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,12 @@
+from accounts.forms import UserChangeForm
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import DetailView
+from django.urls import reverse
+from django.views.generic import DetailView, UpdateView, ListView
 
-from .forms import UserCreationForm
-
+from .forms import UserCreationForm, UserChangePasswordForm
 
 
 def login_view(request):
@@ -40,5 +42,47 @@ class UserDetailView(DetailView):
     model = User
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
+
+class UserChangeView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_update.html'
+    context_object_name = 'user_obj'
+    form_class = UserChangeForm
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_success_url(self):
+        return reverse('accounts:user_detail', kwargs={'pk': self.object.pk})
+
+
+class UserChangePasswordView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_change_password.html'
+    form_class = UserChangePasswordForm
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    # def form_valid(self, form):
+    #     user = form.save()
+    #     login(self.request, user)
+    #     return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('accounts:login')
+
+
+class UsersList(ListView):
+    template_name = 'users_list.html'
+    context_object_name = 'users'
+    model = User
+    paginate_by = 3
+    paginate_orphans = 1
+
+    def get_queryset(self):
+        return User.objects.all()
+
 
 # Create your views here.
