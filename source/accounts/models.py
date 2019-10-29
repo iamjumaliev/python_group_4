05261fbe7e46769242fca87e68114a24d6fdda87
emir <emir.jumaliev@gmail.com>
2019-10-29 11:09:50 -0400

    was made migration and added model UserProfile

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    github = models.CharField(max_length=200,null=False, blank=True,verbose_name='Ссылка на гитхаб')
+
+    def __unicode__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
 # Create your models here.
