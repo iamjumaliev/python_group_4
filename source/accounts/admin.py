7e3d1cfa055c2d649b64dddf7e964e3ab018e2dd
emir <emir.jumaliev@gmail.com>
2019-11-03 12:34:44 -0500

    was configuered accounts/admin.py

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,6 +1,20 @@
-from django.contrib import admin
-
 from accounts.models import UserProfile
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-admin.site.register(UserProfile)
+
+
+
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+    fields = ['birth_date', 'avatar']
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(UserProfile, UserProfileAdmin)
 # Register your models here.
