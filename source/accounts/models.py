6c9ccb5420fb5ca167ee97db7f026e6005d66a70
emir <emir.jumaliev@gmail.com>
2019-11-03 12:15:14 -0500

    was added new fields in UserProfile about,avatar

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -6,7 +6,8 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
     github = models.CharField(max_length=200,null=False, blank=True,verbose_name='Ссылка на гитхаб')
-
+    about = models.TextField(max_length=2000,null=True, blank=True, verbose_name='О себе ')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
     def __str__(self):
         return self.user
 
