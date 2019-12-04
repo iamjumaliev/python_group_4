5ca3fc5d25c2098575b1f29d9f91570471675546
emir <emir.jumaliev@gmail.com>
2019-12-04 02:31:03 -0500

    was added serializers for Project,Mission

---
+++ source/api_v2/serializers.py
@@ -0,0 +1,17 @@
+from webapp.models import Project,Mission
+
+from rest_framework import serializers
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'name', 'description', 'created_at', 'updated_at')
+
+
+class MissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mission
+        fields = ('id', 'summary', 'description', 'status', 'type',
+                  'project','created_at', 'updated_at', 'created_by', 'assigned_to')
+
